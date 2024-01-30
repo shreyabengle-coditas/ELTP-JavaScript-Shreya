c5398db3b2b4dd40871cd41c9e8d390b89ed9277
shreyabengle-coditas <158143330+shreyabengle-coditas@users.noreply.github.com>
2024-01-30 16:27:36 +0530

    Add files via upload

---
+++ AssignmentDayTwo.js
@@ -0,0 +1,29 @@
+//Declaring four variables without assigning values 
+let studentName;
+let studentId;
+let studentCity;
+let studentGender;
+
+//Declaring four variables assigning values
+const employeeName=1;
+const employeeId=2;
+const employeeCity=3;
+const employeeGender=4;
+
+
+//Declare variables to store your first name, last name, marital status, country and age in multiple lines
+const firstName="Shreya";
+const lastName="Bengle";
+const martialStatus="Single";
+const country="India";
+const age=21;
+
+//Declare variables to store your first name, last name, marital status, country and age in a single line
+const firstName1="Shreya", lastName1="Bengle", martialStatus1="Single", country1="India", age1=21;
+
+
+
+//Declare two variables myAge and yourAge and assign them initial values 
+const myAge=21;
+const yourAge=30;
+console.log(myAge, yourAge);
